cfb58bba06f375f8c1cf5fd93a79c66a913607d2
Antoine Herzog <antoine@iov.one>
2020-09-19 16:35:54 +0200

    fix test

--- android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/iov/TestIOVAddress.kt
+++ android/app/src/androidTest/java/com/trustwallet/core/app/blockchains/iov/TestIOVAddress.kt
@@ -4,7 +4,7 @@
 // terms governing use, modification, and redistribution, is contained in the
 // file LICENSE at the root of the source code distribution tree.
 
-package com.trustwallet.core.app.blockchains.bandchain
+package com.trustwallet.core.app.blockchains.iov
 
 import com.trustwallet.core.app.utils.toHex
 import com.trustwallet.core.app.utils.toHexByteArray
@@ -12,7 +12,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import wallet.core.jni.*
 
-class TestBandChainAddress {
+class TestIOVAddress {
 
     init {
         System.loadLibrary("TrustWalletCore")
@@ -23,10 +23,10 @@ class TestBandChainAddress {
 
         val key = PrivateKey("1037f828ca313f4c9e120316e8e9ff25e17f07fe66ba557d5bc5e2eeb7cba8f6".toHexByteArray())
         val publicKey = key.getPublicKeySecp256k1(true)
-        val address = AnyAddress(publicKey, CoinType.BANDCHAIN)
-        val expected = AnyAddress("band1jf9aaj9myrzsnmpdr7twecnaftzmku2mgms4n3", CoinType.BANDCHAIN)
+        val address = AnyAddress(publicKey, CoinType.IOV)
+        val expected = AnyAddress("star1gfpfk3s8xnupdce7pgt3z7mf79clvkw6px89ne", CoinType.IOV)
 
-        assertEquals(publicKey.data().toHex(), "0x035df185566521d6a7802319ee06e1a28e97b7772dfb5fdd13ca6f0575518968e4")
+        assertEquals(publicKey.data().toHex(), "starpub1addwnpepq2svqq4xaxq4jxey3nx69zznxjuhke02fm2q0a45lt5h2uw78n9qq2jpw7g")
         assertEquals(address.description(), expected.description())
     }
 }
